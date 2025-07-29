6590618ba45234ecaae737a0a172d1a780cc9df8
riyaverma85 <riyaverma2976@gmail.com>
2025-07-29 01:41:23 -0700

    css

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
 app.use("/",routeData)
-
+app.use(express.static("./public"))
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
