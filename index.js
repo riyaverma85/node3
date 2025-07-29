ee29cfb291090b9500e4d83078991f8b2fb0e792
riyaverma85 <riyaverma2976@gmail.com>
2025-07-28 22:48:10 -0700

    insex

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 const express=require("express")
 const app=express();
 const  routeData=require("./routes/empRoute")
-// const mongoose=require("mongoose");
-
+const mongoose=require("mongoose");
+const bodyparser=require("body-parser")
+mongoose.connect("mongodb://127.0.0.1:27017/prodb").then(()=>{
+    console.log("database successfully added")
+})
 app.use("/",routeData)
 app.set("view engine","ejs")
+
+//Bodyparser middleware
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
 
 
 app.listen(3000,()=>{
--- models/empModel.js
+++ models/empModel.js
@@ -1,5 +1,5 @@
 
-// const mongoose=require("mongoose");
-// const empSchema=new mongoose.model({
+const mongoose=require("mongoose");
+const empSchema=new mongoose.model({
 
-// })
+})
