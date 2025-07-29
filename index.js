a787bdb34deb6f4de4b4b2aaaf8fd0feb13c5ccb
riyaverma85 <riyaverma2976@gmail.com>
2025-07-28 22:11:04 -0700

    index

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app=express();
 const  routeData=require("./routes/empRoute")
 app.use("/students",routeData)
 
-app.listen(3000,()=>{
+app.listen(4000,()=>{
     console.log("Server is running on port 3000")
 })
